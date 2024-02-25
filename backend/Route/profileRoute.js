4ff13cf6af65c941e2a67daa121303d23e45b544
Dhruvin Pambhar <dhruvinpambhar0609@gmail.com>
2024-02-25 18:35:33 +0530

    user profile completed

--- backend/Route/profileRoute.js
+++ backend/Route/profileRoute.js
@@ -3,10 +3,17 @@ const router = express.Router();
 const User = require('../models/user');
 const Doctor = require('../models/doctorModel');
 const Pharmacist = require('../models/pharmacistModel');
+const mongoose = require('mongoose');
 
-router.post("/updatedoctor" ,async (req, res)=>{
+router.post("/updatedoctor", async (req, res) => {
     try {
-        const doctorId = req.body.id;
+        // console.log(req.body)
+        const userId = req.body.id;
+        // const validUserId = mongoose.Types.ObjectId(userId);
+        const user = await User.findOne({ _id: userId });
+        console.log(user);
+        const doctorId = user.doctor_profile;
+
         const {
             specialization,
             availability,
@@ -18,13 +25,20 @@ router.post("/updatedoctor" ,async (req, res)=>{
 
         // Find the doctor by ID
         let doctor = await Doctor.findById(doctorId);
+
         if (!doctor) {
             return res.status(404).json({ error: 'Doctor not found.' });
         }
-
+        console.log(availability)
+        const convertedAvailability = availability.map(({ day, slots }) => ({
+            day,
+            slots: slots.map(time => ({ time, isBooked: false }))
+            // Add the isBooked field for each time slot
+        }));
+        console.log(convertedAvailability)
         // Update the doctor's information
         doctor.specialization = specialization;
-        doctor.availability = availability;
+        doctor.availability = convertedAvailability;
         doctor.Bio = Bio;
         doctor.Experiance = Experiance;
         doctor.Phone_no = Phone_no;
@@ -32,14 +46,35 @@ router.post("/updatedoctor" ,async (req, res)=>{
 
         // Save the updated doctor information
         await doctor.save();
-
+        console.log(doctor)
         res.status(200).json({ message: 'Doctor information updated successfully.', doctor });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'An error occurred.' });
     }
 });
-router.post('/updatepharmacist',async (req,res)=>{
+
+router.post('/doctor', async (req, res) => {
+    const userId = req.body.id;
+    // const validUserId = mongoose.Types.ObjectId(userId);
+    const user = await User.findOne({ _id: userId });
+    console.log(user);
+    const doctorId = user.doctor_profile;
+    let doctor = await Doctor.findById(doctorId);
+
+    res.status(200).json({ message: 'Doctor send.', doctor, user });
+
+})
+router.post('/user', async (req, res) => {
+    const userId = req.body.id;
+    // const validUserId = mongoose.Types.ObjectId(userId);
+    const user = await User.findOne({ _id: userId });
+    console.log(user);
+
+    res.status(200).json({ message: 'Doctor send.', user });
+
+})
+router.post('/updatepharmacist', async (req, res) => {
     try {
         const pharmacistId = req.body.id;
         const {
